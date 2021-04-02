266d2831bc6fbec5e527aa1709bc913a6cd76468
amandiu <amanullah15-2447@diu.edu.bd>
2021-04-02 19:46:09 +0600

    futtor

--- neoteric/src/component/Footer.js
+++ neoteric/src/component/Footer.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import './Footer.css'
-
+import ReactDOM from 'react-dom'
+import { AiOutlineFacebook } from 'react-icons/ai'
+import { AiOutlineWhatsApp} from 'react-icons/ai'
+import { FaInstagramSquare} from 'react-icons/fa'
+import { AiOutlineMail} from 'react-icons/ai'
 
 function Footer() {
 
     return ( 
         <div className = 'footer-box' >
-            <div className = 'footer-box1' >
-                <button>Open Lab 2 Videco</button>
+            
+             <div className = 'footer-box1' >
+                 <div className = 'bx1'>
+                     <AiOutlineFacebook className = 'f'></AiOutlineFacebook>
+                 </div>
+                 <div className = 'bx1'>
+                 <AiOutlineWhatsApp className = 'f'></AiOutlineWhatsApp>
+                 </div>
+                 <div className = 'bx1'>
+                 <FaInstagramSquare className = 'f'></FaInstagramSquare>
+                 </div>
+                 <div className = 'bx1'>
+                 <AiOutlineMail className = 'f'></AiOutlineMail>
+                 </div>
+              
+             </div>
+            <div className = 'footer-box2' >
             </div>
         </div>
     );
--- neoteric/src/component/LoginBar.js
+++ neoteric/src/component/LoginBar.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import './LoginBar.css'
-import Menu from '@material-ui/core/Menu'
-import MenuItem from '@material-ui/core/MenuItem'
 import {BsThreeDotsVertical} from 'react-icons/bs'
 function LoginBar(props){
     return(
