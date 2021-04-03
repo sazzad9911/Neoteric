2c7e014b8580128fc837ff5b3aeefb2b06e7b225
amandiu <amanullah15-2447@diu.edu.bd>
2021-04-03 18:28:02 +0600

    card

--- neoteric/src/component/sub-component/Card.js
+++ neoteric/src/component/sub-component/Card.js
@@ -2,20 +2,31 @@ import React from 'react'
 import './card.css'
 import x from './../../files/t-shart.jpg'
 
-function card() {
+function Card() {
     return( 
          <div className = 'card-bx' >
          <div className = 'card' >
          <div className = 'imgbx' >
              <img src={x}></img>
+             <div className ='contentBx'>
+             <h2>T-Start</h2>
          <div className = 'size' >
-             <h1>T-Start</h1>
-             <h2>Size</h2>
+             <h3>Size</h3>
              <span>s</span>
              <span>m</span>
              <span>l</span>
              <span>xl</span>
              <span>xxl</span>
+         </div>
+         <div className = 'color' >
+             <h3>color :</h3>
+             <span></span>
+             <span></span>
+             <span></span>
+             <span></span>
+             <span></span>
+        </div>
+        <a href='#'> Buy Now </a>
          </div>
          </div>
          </div>
@@ -23,4 +34,4 @@ function card() {
 );
   
 }
-export default card;
+export default Card;
