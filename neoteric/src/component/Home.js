134a982ef57fcc487abc32a96adfe11ab1b2c901
amandiu <amanullah15-2447@diu.edu.bd>
2021-04-03 17:00:56 +0600

    card

--- neoteric/src/component/Home.js
+++ neoteric/src/component/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import './Home.css'
-import Card from './component/sub-component/Card'
+import Card from './sub-component/Card'
 
 function Home(){
     return(
         <div className='home'>
-             <Slider></Slider>
+            
             <Card></Card>
             
         </div> 
--- neoteric/src/component/sub-component/Card.js
+++ neoteric/src/component/sub-component/Card.js
@@ -1,17 +1,16 @@
 import React from 'react'
 import './card.css'
+import x from './../../files/t-shart.jpg'
 
 function card() {
     return( 
          <div className = 'card-bx' >
          <div className = 'card' >
          <div className = 'imgbx' >
-             <img src='t-shart.png'></img>
-         <div className = 'card-bx1' >
-             <h2>T-Start</h2>
-         </div>
+             <img src={x}></img>
          <div className = 'size' >
-             <h3>Size</h3>
+             <h1>T-Start</h1>
+             <h2>Size</h2>
              <span>s</span>
              <span>m</span>
              <span>l</span>
