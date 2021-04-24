9712498e36fbb14af1c896d2ca30d13ee0bc66e9
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2021-04-24 16:58:01 +0600

    add cart

--- neoteric/src/component/Home.js
+++ neoteric/src/component/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import './Home.css'
 import Card from './sub-component/Card'
 import ImageSlider from './sub-component/ImageSlide'
@@ -6,8 +6,35 @@ import img1 from './../files/ismart-shankar_156195627930.jpg'
 import img2 from './../files/shutterstock_1009843408.jpg'
 import img3 from './../files/shutterstock_554314555_copy.jpg'
 import Slider from './../component/sub-component/Slider'
-
+import firebaseApp from './../firebase'
+import {ShowLoader,HideLoader} from './sub-component/Loader'
+import Carousel from 'react-elastic-carousel';
 function Home(){
+
+    const [recent,setRecent]=useState([]);
+
+    useEffect(()=>{
+        ShowLoader();
+        const db=firebaseApp.firestore();
+        var i=0;
+        var a=[]
+        db.collection("post").orderBy('date','desc').limit(10)
+           .get()
+           .then((querySnapshot) => {
+              querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            a[i]=doc.data();
+            i++;
+        });
+        setRecent(a);
+        HideLoader();
+      })
+        .catch((error) => {
+        console.log("Error getting documents: ", error);
+        HideLoader();
+      });
+    },[])
+
     return(
         <div className='home'>
             <div className='imgslide'>
@@ -15,14 +42,28 @@ function Home(){
             </div>
             <h4>Recent Product :</h4>
             <div className='bx22'>
-                <Card></Card>
-                <Card></Card>
-                
+                <Carousel>
+                {
+                    recent!==null?(
+                        <div>
+                            {
+                                recent.map((d,i)=>(
+                                    <Card data={d}></Card>
+                                ))
+                            }
+                        </div>
+                    ):
+                    (
+                        <div>
+                            <h3>No document to view</h3>
+                        </div>
+                    )
+                }
+                </Carousel>
             </div>
             <h4>Top Product :</h4>
             <div className='bx22'>
-            <Card></Card>
-            <Card></Card>
+           
             </div>
         </div> 
     )
--- neoteric/src/component/sub-component/Card.js
+++ neoteric/src/component/sub-component/Card.js
@@ -2,34 +2,49 @@ import React from 'react'
 import './card.css'
 import x from './../../files/printed-t-shirt-red-png-download-700700-free-transparent-red-t-shirt-png-900_700-removebg-preview.png'
 
-function Card() {
+function Card({data}) {
+    if(data===null){
+        return(
+            <div className='container'>
+                <div className='card'>
+                    <div className='contentBx'>
+                        <h3>No such as data!</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else{
     return( 
          <div className='container'>
              <div className='card' >
                  <div className='imgBx'>
-                     <img src={x} alt='n'></img>
+                     <img src={data.img} alt='n'></img>
                  </div>
                  <div className='contentBx'>
-                     <h2>Nick Shoes</h2>
-                     <h3>Prize: </h3>
+                     <h2>{data.name}</h2>
+                     <h3>Prize: {data.prize}</h3>
                      <div className='size'>
                          <h3>Size :</h3>
-                         <span>S</span>
-                         <span>M</span>
-                         <span>L</span>
-                         <span>XL</span>
+                         {
+                             data.size.map((d,i)=>(
+                                <span>{d}</span>
+                             ))
+                         }
                      </div>
                      <div className='colorr'>
                          <h3>Color :</h3>
-                         <span></span>
-                         <span></span>
-                         <span></span>
+                         {
+                             data.color.map((dat,i)=>(
+                                 <span style={{background: dat}}></span>
+                             ))
+                         }
                      </div>
                      <a>View Now</a>
                  </div>
              </div>
          </div>
 );
-  
+    }
 }
 export default Card;
